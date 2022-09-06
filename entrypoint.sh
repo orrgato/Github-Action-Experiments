ccfb9b22515012cd6f109ddffe34c5e38acab67c
Shivam Verma <89532792+orrgato@users.noreply.github.com>
2022-09-06 22:54:07 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -l
 
 echo "Alright This Works"
-echo "$GITHUB_REF"
+echo "$GITHUB_SHA"
 echo "$GITHUB_REPOSITORY"
 echo "$GITHUB_WORKFLOW"
-echo "$GITHUB_HEAD_REF"
-echo "$GITHUB_ACTION_REPOSITORY"
-echo "$GITHUB_PATH"
+echo "$GITHUB_BASE_REF"
+echo "$GITHUB_ACTION"
+echo "$GITHUB_WORKSPACE"
